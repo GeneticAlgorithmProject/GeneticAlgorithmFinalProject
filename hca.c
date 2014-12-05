79d1fcd0c999121c8346c7e36dcb070da17cd43a
asymptoter <a84784480@hotmail.com>
2014-12-05 17:41:09 +0800

    Update hca.c

--- hca.c
+++ hca.c
@@ -2,36 +2,92 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-void printResult(int *p[])
+void printResult(int *p,int v)
 {}
-bool condition(int *p[])
+bool condition(int *p,int v)
 {}
-void greedySearch(int p[],int s)
+void greedySearch(int *p,int *g,int v,int random)
 {}
-void selectParents(int *p[],int *p1,int *p2)
+void selectParents(int *p,int *g,int v,int *p1,int *p2)
 {}
-void crossover(int *p1,int *p2,int *c1,int *c2)
+void crossover(int *g,int *p1,int *p2,int *c1,int *c2)
 {}
-void localSearch(int *c)
+void localSearch(int *p,int *g,int *c)
 {}
-void updatePopulation(int *p[],int *c1,int *c2)
+void updatePopulation(int *p,int v,int *c1,int *c2)
 {}
-int fileProcessing1(char *ptr)
+int fileProcessing1(char *ptr)//get v
 {
-
+	FILE *fp;
+	char temp[100];
+	fp=fopen(ptr,"r");
+	if(fp==NULL)perror("Error opening file.");
+	else
+	{
+		while(!feof(fp))
+		{
+			if((fgets(temp,100,fp)!=NULL)&&(strcmp(temp,"p")==1))
+			{
+				char *tt;
+				tt=strtok(temp," ");
+				tt=strtok(NULL," ");
+				if(strcmp(tt,"edge")==1)
+				{
+					//???
+				}
+				else
+				{
+					tt=strtok(NULL," ");
+					fclose(fp);
+					return(atoi(tt));
+				}
+			}
+		}
+	}
 }
-void fileProcessing2(int *f[])
+void fileProcessing2(int *g,int v,char *ptr)//get graph (matrix)
 {
-
+	FILE *fp;
+	char temp[100];
+	fp=fopen(ptr,"r");
+	if(fp==NULL)perror("Error opening file.");
+	else
+	{
+		while(!feof(fp))
+		{
+			fgets(temp,100,fp);
+			char *tt,*ss;
+			tt=strtok(temp," ");
+			if(strcmp(tt,"e")==0)
+			{
+				tt=strtok(NULL," ");
+				ss=strtok(NULL," ");
+				int t=atoi(tt);
+				int s=atoi(ss);
+				printf("t=%d s=%d\n",t,s);
+				*(g+t*v+s)=1;//bug in here?
+				
+			}
+		}
+		int i,j;
+		for(i=0;i<v;i++)
+		{
+			for(j=0;j<v;j++)
+			{
+				printf("g[%d][%d]=%d\n",i,j,*(g+i*v+j));
+			}
+		}
+		fclose(fp);
+	}
 }
-void init(int *p[],int n,int v)
+void init(int *p,int *g,int n,int v)
 {
     int i;
     for(i=0;i<n;i++)
     {
         srand(time(NULL));
         int random=rand()%v;
-        greedySearch(p[i],random);//ramdom select a vertex as starting point to color
+        greedySearch(p,g,v,random);//ramdom select a vertex as starting point to color
     }
 
 }
@@ -42,30 +98,31 @@ int main(int argc,char *argv[])
         printf("population_size repeat_times localsearch_length file_name chromatic_number\n");
         return(-1);
     }
-    int n=atoi(argv[1]);
-    int r=atoi(argv[2]);
-    int lsl=atoi(argv[3]);
-    char *ptr=argv[4];
-    int k=atoi(argv[5]);
+    int n=atoi(argv[1]);//population size
+    int r=atoi(argv[2]);//repeat times
+    int lsl=atoi(argv[3]);//localsearch length
+    char *ptr=argv[4];//file name
+    int k=atoi(argv[5]);//chromatic number
 
     //printf("%d %d %d %s %d\n",n,r,lsl,ptr,k);
     int v=fileProcessing1(ptr);//get vertices number
-    int *G[v];
-    fileProcessing2(G);//get graph
-    int *p[n];//n population, v = chromosome_length = vertices_number
+    int *g=(int*)malloc(sizeof(int)*v*(v+1));
+    fileProcessing2(g,v,ptr);//get graph (matrix)
+	printf("fileProcessing2 successful!\n");
+    int *p;//n population, v = chromosome_length = vertices_number
     while(r>0)//repeat r times
     {
-        init(p,n,v);
-        while(condition(p)==true)
+        init(p,g,n,v);
+        while(condition(p,v)==true)
         {
             int p1[v],p2[v],c1[v],c2[v];
-            selectParents(p,p1,p2);
-            crossover(p1,p2,c1,c2);
-            localSearch(c1);
-            localSearch(c2);
-            updatePopulation(p,c1,c2);
+            selectParents(p,g,v,p1,p2);
+            crossover(g,p1,p2,c1,c2);
+            localSearch(p,g,c1);
+            localSearch(p,g,c2);
+            updatePopulation(p,v,c1,c2);
         }
-        printResult(p);//need to implement; 
+        printResult(p,v);//need to implement; 
     }
     return(0);
 }
