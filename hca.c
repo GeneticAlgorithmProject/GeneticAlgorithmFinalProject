7bc302a9de49acf9daf796aeb5b3312293371571
entryword <entryword@gmail.com>
2014-12-09 02:48:54 +0800

    Update hca.c

--- hca.c
+++ hca.c
@@ -2,315 +2,345 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-void localSearch(int *p,int *g,int *c)
+void localSearch(int *p,int *g, int v, int k, int *c)
 {
-
+    int i, j;
+    //int *g=(int*)malloc(sizeof(int)*(v+1)*(v+1));
+    int *t=(int*)malloc(sizeof(v));//check conflict
+    int max=0;//max conflict
+    int maxi=0;//max conflict index
+    for (i=0; i<v; i++) {
+        for (j=0; j<(*g)/v; j++) {
+            if (*g+i*v+j == 1) {
+                if (c[i]==c[j]) {
+                    t[i]++;
+                    t[j]++;
+                }
+            }
+        }
+    }
+    for (i=0; i<v; i++) {
+        if (t[i]>=max) {
+            max=t[i];
+            maxi=i;
+        }
+    }
+    c[i]=(rand() %k) +1;
 }
 void paste(int *p,int p1,int *c1,int v)
 {
-	int i;
-	for(i=0;i<=v;i++)
-	{
-		*(p+p1*(v+1)+i)=*(c1+i);
-	}
+    int i;
+    for(i=0;i<=v;i++)
+    {
+        *(p+p1*(v+1)+i)=*(c1+i);
+    }
 }
 void updatePopulation(int *p,int p1,int p2,int *c1,int *c2,int v)
 {
-	if((*c1)<(*(p+p1*(v+1))))
-	{
-		paste(p,p1,c1,v);
-		if((*c2)<(*(p+p2*(v+1))))
-		{
-			paste(p,p2,c2,v);
-		}
-	}
-	else if((*c2)<(*(p+p1*(v+1))))
-	{
-		paste(p,p1,c2,v);
-		if((*c1)<(*(p+p2*(v+1))))
-		{
-			paste(p,p2,c1,v);
-		}
-	}
-	else;
+    if((*c1)<(*(p+p1*(v+1))))
+    {
+        paste(p,p1,c1,v);
+        if((*c2)<(*(p+p2*(v+1))))
+        {
+            paste(p,p2,c2,v);
+        }
+    }
+    else if((*c2)<(*(p+p1*(v+1))))
+    {
+        paste(p,p1,c2,v);
+        if((*c1)<(*(p+p2*(v+1))))
+        {
+            paste(p,p2,c1,v);
+        }
+    }
+    else;
 }
 int getBestFitness(int *p,int v,int n)
 {
-	int i,min=*p;
-	int who=0;
-	for(i=1;i<n;i++)
-	{
-		if(min>*(p+i*(v+1)))
-		{
-			min=*(p+i*(v+1));
-			who=i;
-		}
-	}
-	return(who);
+    int i,min=*p;
+    int who=0;
+    for(i=1;i<n;i++)
+    {
+        if(min>*(p+i*(v+1)))
+        {
+            min=*(p+i*(v+1));
+            who=i;
+        }
+    }
+    return(who);
 }
 double getAverageFitness(int *p,int v,int n)
 {
-	int i;
-	double sum=0.0;
-	for(i=0;i<n;i++)
-	{
-		sum+=(double)*(p+i*(v+1));
-	}
-	sum/=n;
-	return(sum);
+    int i;
+    double sum=0.0;
+    for(i=0;i<n;i++)
+    {
+        sum+=(double)*(p+i*(v+1));
+    }
+    sum/=n;
+    return(sum);
 }
 void printResult(int *p,int v,int n)
 {
-	int best=getBestFitness(p,v,n);
-	printf("Avg=%lf Best=%d ",getAverageFitness(p,v,n),*(p+best*(v+1)));
-	int i;
-	for(i=0;i<=v;i++)
-	{
-		printf("%d",*(p+best*(v+1)+i));
-	}
-	printf("\n");
+    int best=getBestFitness(p,v,n);
+    printf("Avg=%lf Best=%d ",getAverageFitness(p,v,n),*(p+best*(v+1)));
+    int i;
+    for(i=0;i<=v;i++)
+    {
+        printf("%d",*(p+best*(v+1)+i));
+    }
+    printf("\n");
 }
 void printAverageFitness(int *p,int v,int n)
 {
-	printf("Average Fitness=%lf\n",getAverageFitness(p,v,n));
+    printf("Average Fitness=%lf\n",getAverageFitness(p,v,n));
 }
 int fitness(int *g,int *p,int v,int x)
 {
-	int i,j;
-	int f=0;
-	for(i=1;i<=v;i++)
-	{
-		for(j=1;j<=v;j++)
-		{
-			if(i==j)continue;
-			if((*(g+i*(v+1)+j)==1)&&(*(p+x*(v+1)+i)==*(p+x*(v+1)+j)))
-			{
-				f++;
-				break;
-			}
-		}
-	}
-	return(f);
+    int i,j;
+    int f=0;
+    for(i=1;i<=v;i++)
+    {
+        for(j=1;j<=v;j++)
+        {
+            if(i==j)continue;
+            if((*(g+i*(v+1)+j)==1)&&(*(p+x*(v+1)+i)==*(p+x*(v+1)+j)))
+            {
+                f++;
+                break;
+            }
+        }
+    }
+    return(f);
 }
 void simpleCrossover(int *g,int *p,int p1,int p2,int *c1,int *c2,int v)
 {
-	int i,r;
-	srand(time(NULL));
-	for(i=1;i<=v;i++)
-	{
-		r=rand()%2;
-		if(r==0)
-		{
-			*(c1+i)=*(p+p1*(v+1)+i);
-			*(c2+i)=*(p+p2*(v+1)+i);
-		}
-		else
-		{
-			*(c1+i)=*(p+p2*(v+1)+i);
-			*(c2+i)=*(p+p1*(v+1)+i);
-		}
-	}
-	*(c1)=fitness(g,c1,v,0);
-	*(c2)=fitness(g,c2,v,0);
+    int i,r;
+    srand(time(NULL));
+    for(i=1;i<=v;i++)
+    {
+        r=rand()%2;
+        if(r==0)
+        {
+            *(c1+i)=*(p+p1*(v+1)+i);
+            *(c2+i)=*(p+p2*(v+1)+i);
+        }
+        else
+        {
+            *(c1+i)=*(p+p2*(v+1)+i);
+            *(c2+i)=*(p+p1*(v+1)+i);
+        }
+    }
+    *(c1)=fitness(g,c1,v,0);
+    *(c2)=fitness(g,c2,v,0);
 }
 void randomSelectParents(int n,int *p1,int *p2)
 {
-	srand(time(NULL));
-	*p1=rand()%n;
-	*p2=rand()%n;
-	while(*p1==*p2)
-	{
-		*p2=rand()%n;
-	}
+    srand(time(NULL));
+    *p1=rand()%n;
+    *p2=rand()%n;
+    while(*p1==*p2)
+    {
+        *p2=rand()%n;
+    }
 }
 int condition(int *p,int v,int n)
 {
-	int i;
-	for(i=0;i<n;i++)
-	{
-		if(*(p+i*(v+1))==0)
-		{
-			return(0);
-		}
-	}
-	return(1);
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(*(p+i*(v+1))==0)
+        {
+            return(0);
+        }
+    }
+    return(1);
 }
 int fileProcessing1(char *ptr)//get v
 {
-	FILE *fp;
-	char temp[100];
-	fp=fopen(ptr,"r");
-	if(fp==NULL)perror("Error opening file.");
-	else
-	{
-		while(!feof(fp))
-		{
-			if((fgets(temp,100,fp)!=NULL)&&(strcmp(temp,"p")==1))
-			{
-				char *tt;
-				tt=strtok(temp," ");
-				tt=strtok(NULL," ");
-				if(strcmp(tt,"edge")==1)
-				{
-					//???
-				}
-				else
-				{
-					tt=strtok(NULL," ");
-					fclose(fp);
-					return(atoi(tt));
-				}
-			}
-		}
-	}
+    FILE *fp;
+    char temp[100];
+    fp=fopen(ptr,"r");
+    if(fp==NULL){
+        perror("Error opening file.");
+        return 0;
+    }
+    else
+    {
+        if(!feof(fp))
+        {
+            if((fgets(temp,100,fp)!=NULL)&&(strcmp(temp,"p")==1))
+            {
+                char *tt;
+                tt=strtok(temp," ");
+                tt=strtok(NULL," ");
+                if(strcmp(tt,"edge")==1)
+                {
+                    //???
+                    return 0;
+                }
+                else
+                {
+                    tt=strtok(NULL," ");
+                    fclose(fp);
+                    return(atoi(tt));
+                }
+            }
+            else {
+                return 0;
+            }
+        }
+        else
+            return 0;
+    }
 }
 void fileProcessing2(int *g,int v,char *ptr)//get graph (matrix)
 {
-	FILE *fp;
-	char temp[100];
-	fp=fopen(ptr,"r");
-	if(fp==NULL)perror("Error opening file.");
-	else
-	{
-		int i,j;
-		for(i=0;i<=v;i++)
-		{
-			*(g+i*(v+1))=v;
-			for(j=1;j<=v;j++)
-			{
-				*(g+i*(v+1)+j)=0;
-			}
-		}
-		while(!feof(fp))
-		{
-			if(fgets(temp,100,fp)==NULL)
-			{
-				break;
-			}
-			char *tt,*ss;
-			tt=strtok(temp," ");
-			if(strcmp(tt,"e")==0)
-			{
-				tt=strtok(NULL," ");
-				ss=strtok(NULL," ");
-				int t=atoi(tt);
-				int s=atoi(ss);
-				//printf("t=%d s=%d\n",t,s);
-				*(g+t*(v+1)+s)=1;
-				//printf("g[%d][%d]=%d\n",t,s,*(g+t*v+s));
-			}
-		}
-		fclose(fp);
-	}
+    FILE *fp;
+    char temp[100];
+    fp=fopen(ptr,"r");
+    if(fp==NULL)perror("Error opening file.");
+    else
+    {
+        int i,j;
+        for(i=0;i<=v;i++)
+        {
+            *(g+i*(v+1))=v;
+            for(j=1;j<=v;j++)
+            {
+                *(g+i*(v+1)+j)=0;
+            }
+        }
+        while(!feof(fp))
+        {
+            if(fgets(temp,100,fp)==NULL)
+            {
+                break;
+            }
+            char *tt,*ss;
+            tt=strtok(temp," ");
+            if(strcmp(tt,"e")==0)
+            {
+                tt=strtok(NULL," ");
+                ss=strtok(NULL," ");
+                int t=atoi(tt);
+                int s=atoi(ss);
+                //printf("t=%d s=%d\n",t,s);
+                *(g+t*(v+1)+s)=1;
+                //printf("g[%d][%d]=%d\n",t,s,*(g+t*v+s));
+            }
+        }
+        fclose(fp);
+    }
 }
 int findDegree(int *g,int v,int l)
 {
-	int i;
-	int degree=0;
-	for(i=1;i<=v;i++)
-	{
-		degree+=*(g+l*(v+1)+i);
-	}
-	return(degree);
+    int i;
+    int degree=0;
+    for(i=1;i<=v;i++)
+    {
+        degree+=*(g+l*(v+1)+i);
+    }
+    return(degree);
 }
 int findMaxDegree(int *g,int v,int *who)
 {
-	int i;
-	int degree;
-	int	max_degree=findDegree(g,v,*who);
-	for(i=1;i<=v;i++)
-	{
-		degree=findDegree(g,v,i);
-		if(max_degree<degree)
-		{
-			max_degree=degree;
-			*who=i;
-		}
-	}
-	return(max_degree);
+    int i;
+    int degree;
+    int	max_degree=findDegree(g,v,*who);
+    for(i=1;i<=v;i++)
+    {
+        degree=findDegree(g,v,i);
+        if(max_degree<degree)
+        {
+            max_degree=degree;
+            *who=i;
+        }
+    }
+    return(max_degree);
 }
 void printPopulation(int *p,int n,int v)
 {
-	int i,j;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<=v;j++)
-		{
-			printf("%d",*(p+i*v+j));
-		}
-		printf("\n");
-	}
-	printf("\n");
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=v;j++)
+        {
+            printf("%d",*(p+i*v+j));
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 void init(int *p,int *g,int n,int v,int k)//do greedy search
 {
     int i,j,l,m;
-	srand(time(NULL));
-	int max_degree=0;
-	int max_who=0;
-	max_degree=findMaxDegree(g,v,&max_who);
-	
+    srand(time(NULL));
+    int max_degree=0;
+    int max_who=0;
+    max_degree=findMaxDegree(g,v,&max_who);
+    
     for(i=0;i<n;i++)
     {
         //srand(time(NULL));
         int random;
-		int record[v+1];
-		int color[k+1];
-		for(l=1;l<=v;l++)
-		{
-			record[l]=0;
-			*(p+i*(v+1)+l)=0;
-		}
-		*(p+i*(v+1)+max_who)=1;//the vertex with max degree color 1
-		record[max_who]=1;
-		for(j=1;j<v;j++)
-		{
-			for(m=1;m<=k;m++)
-			{
-				color[m]=0;
-			}
-			//random select a vertex to color
-			int r;
-			r=rand()%v+1;
-			while(record[r]==1)
-			{
-				r=r%v+1;
-			}
-		
-			//check the neighbor adjacent to this vertex if colored
-			for(m=1;m<=v;m++)
-			{
-				if((*(g+r*(v+1)+m)==1)&&(record[m]==1))//adjacent and colored neighbor
-				{
-					color[(*(p+i*(v+1)+m))]=1;
-				}
-				else if(*(g+r*(v+1)+m)==0)//not adjacent 
-				{
-					color[(*(p+i*(v+1)+m))]=-1;//ignore
-				}
-				else;//adjacent but not colored keep 0
-			}
-
-			int flag=0;
-			for(m=1;m<=k;m++)
-			{
-				if(color[m]==0)//this color is not appeared in neighbor
-				{
-					*(p+i*(v+1)+r)=m;
-					flag=1;
-					break;
-				}
-			}
-			if(flag==0)//color is exhausted
-			{
-				random=rand()%k+1;
-				*(p+i*(v+1)+r)=random;
-			}
-			record[r]=1;
-		}
-		*(p+i*(v+1))=fitness(g,p,v,i);
+        int record[v+1];
+        int color[k+1];
+        for(l=1;l<=v;l++)
+        {
+            record[l]=0;
+            *(p+i*(v+1)+l)=0;
+        }
+        *(p+i*(v+1)+max_who)=1;//the vertex with max degree color 1
+        record[max_who]=1;
+        for(j=1;j<v;j++)
+        {
+            for(m=1;m<=k;m++)
+            {
+                color[m]=0;
+            }
+            //random select a vertex to color
+            int r;
+            r=rand()%v+1;
+            while(record[r]==1)
+            {
+                r=r%v+1;
+            }
+            
+            //check the neighbor adjacent to this vertex if colored
+            for(m=1;m<=v;m++)
+            {
+                if((*(g+r*(v+1)+m)==1)&&(record[m]==1))//adjacent and colored neighbor
+                {
+                    color[(*(p+i*(v+1)+m))]=1;
+                }
+                else if(*(g+r*(v+1)+m)==0)//not adjacent
+                {
+                    color[(*(p+i*(v+1)+m))]=-1;//ignore
+                }
+                else;//adjacent but not colored keep 0
+            }
+            
+            int flag=0;
+            for(m=1;m<=k;m++)
+            {
+                if(color[m]==0)//this color is not appeared in neighbor
+                {
+                    *(p+i*(v+1)+r)=m;
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)//color is exhausted
+            {
+                random=rand()%k+1;
+                *(p+i*(v+1)+r)=random;
+            }
+            record[r]=1;
+        }
+        *(p+i*(v+1))=fitness(g,p,v,i);
     }
-	printPopulation(p,n,v);
+    printPopulation(p,n,v);
 }
 int main(int argc,char *argv[])
 {
@@ -324,7 +354,7 @@ int main(int argc,char *argv[])
     int lsl=atoi(argv[3]);							//localsearch length
     char *ptr=argv[4];								//file name
     int k=atoi(argv[5]);							//chromatic number
-
+    
     int v=fileProcessing1(ptr);						//get vertices number
     int *g=(int*)malloc(sizeof(int)*(v+1)*(v+1));
     fileProcessing2(g,v,ptr);						//get graph (matrix)
@@ -337,13 +367,13 @@ int main(int argc,char *argv[])
             int p1,p2,c1[v],c2[v];
             randomSelectParents(n,&p1,&p2);
             simpleCrossover(g,p,p1,p2,c1,c2,v);
-            //localSearch(p,g,c1);
+            localSearch(p,g, v, k, c1);
             //localSearch(p,g,c2);
             updatePopulation(p,p1,p2,c1,c2,v);
-			//printPopulation(p,n,v);
-			printResult(p,v,n);
+            //printPopulation(p,n,v);
+            printResult(p,v,n);
         }
-		repeat--;
+        repeat--;
     }
     return(0);
 }
